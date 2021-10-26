f64bb168c15497fdb78154d634dd0d45fde6678b
Burak Halefoğlu <42536490+burakhalefoglu@users.noreply.github.com>
2021-10-26 15:08:32 +0300

    refactoring
    refactoring

--- pkg/jsonParser/gojson/jsonparser.go
+++ pkg/jsonParser/gojson/jsonparser.go
@@ -3,17 +3,14 @@ package gojson
 import (
 	"errors"
 	"github.com/goccy/go-json"
-	"log"
 )
 
 type GoJson struct {
 }
 
-
 func (g *GoJson) EncodeJson(v interface{}) (*[]byte, error) {
 	value, err := json.Marshal(&v)
 	if err != nil {
-		log.Printf("Can not marshal Value")
 		return nil, errors.New("Can not marshal Value")
 	}
 	return &value, nil
@@ -22,7 +19,6 @@ func (g *GoJson) EncodeJson(v interface{}) (*[]byte, error) {
 func (g *GoJson) DecodeJson(message *[]byte, v interface{}) error {
 	err := json.Unmarshal(*message, &v)
 	if err != nil {
-		log.Printf("Can not unmarshal JSON")
 		return errors.New("Can not unmarshal JSON")
 	}
 	return nil
