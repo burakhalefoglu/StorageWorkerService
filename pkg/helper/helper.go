a7eb1dad318bf29c10eff873c28a16fd9cd79a30
Burak Halefoğlu <42536490+burakhalefoglu@users.noreply.github.com>
2022-01-23 12:30:31 +0300

    Update helper.go

--- pkg/helper/helper.go
+++ pkg/helper/helper.go
@@ -19,17 +19,22 @@ func CreateHealthFile() {
 	if os.IsNotExist(err) {
 		var file, err = os.Create(path)
 		if isError(err) {
+			fmt.Println("File Created Failed", path)
 			return
 		}
 		defer file.Close()
+		fmt.Println("File Created Successfully", path)
 	}
 }
 
 func DeleteHealthFile() {
 	var err = os.Remove(path)
 	if isError(err) {
+		fmt.Println("File deleting failed")
 		return
 	}
+	fmt.Println("File Deleted")
+
 }
 
 func isError(err error) bool {
